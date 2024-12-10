e76ba541e5ed26edac7f1b1108c39b553f299d82
lucas-bernardo-souza <lucasbernardodesouza510@gmail.com>
2024-12-10 17:47:26 -0300

    Implementação do script na página inicial que realiza o roteamento dos links para as outras páginas e finalização da estilização

---
+++ script.js
@@ -0,0 +1,11 @@
+document.getElementById('regex').addEventListener('click', function(){
+    window.location.assign("./simulador_expressoes_regulares/index.html");
+})
+
+document.getElementById('gramatica').addEventListener('click', function(){
+    window.location.assign("./gramatica_regular/index.html");
+})
+
+document.getElementById('automato').addEventListener('click', function(){
+    window.location.assign("./automatos_finitos/index.html");
+})
